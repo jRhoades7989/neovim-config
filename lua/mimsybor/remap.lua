b138db4463b57f0195e5ac5efa086dbb2f4b440f
Johnathan Rhoades <jrhoades7989@gmail.com>
2024-05-27 16:44:57 -0400

    Added <leader>o mapping for opening closures

--- lua/mimsybor/remap.lua
+++ lua/mimsybor/remap.lua
@@ -7,6 +7,7 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>o', 'i<Cr><Esc>O', {desc = '[O]pen enclosure'})
 
 vim.keymap.set("i", "kj", "<Esc>")
 vim.keymap.set("i", "lkj", "<Esc>:w<Cr>")
